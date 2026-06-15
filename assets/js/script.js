93ec8c81eb7a3723bea351a9715536f56bcd2860
JusTMeth25 <lorenzo.melis@yahoo.it>
2026-06-15 15:34:19 +0200

    create classi e form

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,17 +8,38 @@
 //
 // Bonus: bottone "Segna come letto" su ogni elemento, gestito con event delegation.
 
-
 // === Classi ===
+class Libro {
+  #contatore = 0;
+  constructor(_titolo, _autore, _anno) {
+    this.titolo = _titolo;
+    this.autore = _autore;
+    this.anno = _anno;
+    this.letto = false;
+    this.id = this.#contatore++;
+  }
 
+  segnaComeLetto() {
+    this.letto = true;
+  }
 
+  formato() {
+    return "cartaceo";
+  }
+}
+
+class LibroDigitale extends Libro {
+  constructor(_titolo, _autore, _anno, _dimensioneMb) {
+    super(_titolo, _autore, _anno);
+    this.dimensioneMb = _dimensioneMb;
+  }
+  formato() {
+    return ` digitale(${this.dimensioneMb}MB) `;
+  }
+}
 
 // === Stato (array di libri) ===
 
-
-
 // === Render ===
-
-
 
 // === Eventi ===
